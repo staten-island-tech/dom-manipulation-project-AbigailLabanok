26c6c88e4f86934d3dffb5b9260e7c63b87367d4
AbigailLabanok <abigail.labanok@gmail.com>
2022-11-03 13:15:24 -0400

    dfgh

---
+++ notes.js
@@ -0,0 +1,3 @@
+const button = document.getElementById("btn"); //you can use getElementById when selecting an element by its id
+const text = document.querySelector("#text"); //you can use querySelector when selecting an element by its class
+console.log(button);
