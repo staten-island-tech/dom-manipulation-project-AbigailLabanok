19d4220dfe5b53fbb8a5c3e3a7e83ff20461fccb
AbigailLabanok <abigail.labanok@gmail.com>
2022-11-04 13:17:22 -0400

    sda

--- notes.js
+++ notes.js
@@ -1,8 +1,22 @@
 const DOMSelectors = {
-    button: document.getElementById("btn"), //you can use getElementById when selecting an element by its id
-    text: document.querySelector("#text"), //you can use querySelector when selecting an element by its class
-    point: document.querySelector(".point"),
+  button: document.getElementById("btn"), // getElementById looks for the id that is passed through it, in this case being "btn"
+  text: document.querySelector("#text"), // queryselectors look for elements based on parameters, being '.' or '#'
+  box: document.getElementById("big-black-box"),
+  points: document.querySelectorAll(".point"),
+};
+console.log(DOMSelectors.pointTwo);
+
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "red";
+  text.innerHTML = "This is now a big red box";
+  text.style.fontSize = "4rem";
 }
-console.log(DOMSelectors.point);
+
+backgroundAndText(DOMSelectors.box, DOMSelectors.text);
 
 //you can get elements from html into js even if they do not exist
+//you can use getElementById when selecting an element by its id//you can use querySelector when selecting an element by its class
+//css is the styling so stating .style acceses the style of an element
+//when changing what the text says, you'd use innerHTML instead of style
+//you can add and remove classes using java
+//node list - an array like type/list
