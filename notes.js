c73afdfeab1cd40fee3218b31e19a272405f73f4
AbigailLabanok <abigail.labanok@gmail.com>
2022-11-04 12:49:19 -0400

    a

--- notes.js
+++ notes.js
@@ -1,3 +1,8 @@
-const button = document.getElementById("btn"); //you can use getElementById when selecting an element by its id
-const text = document.querySelector("#text"); //you can use querySelector when selecting an element by its class
-console.log(button);
+const DOMSelectors = {
+    button: document.getElementById("btn"), //you can use getElementById when selecting an element by its id
+    text: document.querySelector("#text"), //you can use querySelector when selecting an element by its class
+    point: document.querySelector(".point"),
+}
+console.log(DOMSelectors.point);
+
+//you can get elements from html into js even if they do not exist
