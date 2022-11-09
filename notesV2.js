7975de77e04eb997bd572a7dcf0ad9b7009e0161
AbigailLabanok <abigail.labanok@gmail.com>
2022-11-09 13:15:07 -0500

    Update notesV2.js

--- notesV2.js
+++ notesV2.js
@@ -3,5 +3,10 @@ const DOMSelectors = {
   box: document.getElementById("container-box"),
   input: document.querySelector(`#input`),
 };
-const cat = "Meow";
-DOMSelectors.box.insertAdjacentHTML("afterend", `<h1>We are an ${cat}</h1>`);
+/* const cat = "Meow";
+DOMSelectors.box.insertAdjacentHTML("afterend", `<h1>We are an ${cat}</h1>`); */
+DOMSelectors.button.addEventListener("click", function () {
+  let input = DOMSelectors.input.value; //used toget the value of the input tg (textbox)
+  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
+  DOMSelectors.input.value = ""; //you can reset values using an empty string
+});
