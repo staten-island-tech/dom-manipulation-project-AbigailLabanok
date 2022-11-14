a8ea8681cdf8c0b175406a214d484593ab47aaaf
AbigailLabanok <abigail.labanok@gmail.com>
2022-11-14 13:14:41 -0500

    sdfg

---
+++ index.js
@@ -0,0 +1,7 @@
+const DOMSelectors = {
+    button: document.querySelector("btn"),
+    title: document.getElementById("title"),
+    artist: document.getElementById("artist"),
+    url: document.getElementById("url"),
+    display: document.getElementById("display")
+}
