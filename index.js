700f68a3115e563fc70f6cf35ec6a0cb5fe3cc4a
AbigailLabanok <abigail.labanok@gmail.com>
2022-11-16 13:15:09 -0500

    erfytguhjnk

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const DOMSelectors = {
-    button: document.querySelector("btn"),
-    title: document.getElementById("title"),
-    artist: document.getElementById("artist"),
-    url: document.getElementById("url"),
-    display: document.getElementById("display")
+  button: document.querySelector(".btn"),
+  title: document.getElementById("title"),
+  artist: document.getElementById("artist"),
+  url: document.getElementById("url"),
+  display: document.getElementById("display"),
+};
+
+function makeCard(backgroundimage, text) {
+  backgroundimage.style.backgroundImage = `${DOMSelectors.url}`;
+  text.innerHTML = `${DOMSelectors.title}`;
+  text.innerHTML = `${DOMSelectors.artist}`;
 }
+
+DOMSelectors.button.addEventListener("click", function () {
+  makeCard(DOMSelectors.display);
+});
