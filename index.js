81fd9c99047f71b04161e4941c5c650ab2d45fcc
AbigailLabanok <abigail.labanok@gmail.com>
2022-11-20 23:54:52 -0500

    sdfsdg

--- index.js
+++ index.js
@@ -4,16 +4,38 @@ const DOMSelectors = {
   artist: document.getElementById("artist"),
   url: document.getElementById("url"),
   display: document.getElementById("display"),
+  removeButton: document.querySelector(".remove btn"),
+  displayCard: document.querySelector(".display-card"),
 };
-/*  
-function makeCard(background, title, artist) {
-  background.style.backgroundImage = `${DOMSelectors.url}`;
-  title.textContent = `${DOMSelectors.title}`;
-  artist.textContent = `${DOMSelectors.artist}`;
-} 
- */
-DOMSelectors.button.addEventListener("click", function () {
-  DOMSelectors.display.insertAdjacentHTML("afterend", 
-  `<p>${title}</p>
-  <p>${artist}</p>`)
-}); 
+
+function makeCard() {
+  let title = DOMSelectors.title.value;
+  let artist = DOMSelectors.artist.value;
+  let url = DOMSelectors.url.value;
+  const removeButton = document.querySelector(".remove btn");
+  const displayCard = document.querySelector(".display-card");
+  DOMSelectors.display.insertAdjacentHTML(
+    "afterend",
+    `<div class="display-card">
+          <img class="display-img" src="${url}"/>
+          <h2 class="display-album">${title}</h2>
+          <h2 class="display-artist"> by ${artist}</h2>
+          <button class="remove btn">Remove Album</button>
+      </div>`
+  );
+  DOMSelectors.title.value = "";
+  DOMSelectors.artist.value = "";
+  DOMSelectors.url.value = "";
+  function removeCard() {
+    DOMSelectors.display.removeChild(displayCard);
+  }
+  DOMSelectors.removeButton.addEventListener("click", removeCard);
+}
+
+/* function removeCard() {
+  DOMSelectors.display.removeChild(displayCard);
+} */
+
+DOMSelectors.button.addEventListener("click", makeCard);
+
+/* DOMSelectors.removeButton.addEventListener("click", removeCard); */
